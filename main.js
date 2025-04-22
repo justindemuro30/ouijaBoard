44b26ba1b82937b937d835262be0e9b6540cb3a7
justindemuro30 <jdemuro30@gmail.com>
2025-04-22 10:55:15 -0400

    adding index.html, main.js and style.css to project

---
+++ main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const result = document.getElementById('result');
+
+    document.getElementById('kids').addEventListener('click', () => {
+        const kids = Math.floor(Math.random() * 21);
+        result.textContent = `You will have ${kids} kids.`;
+    });
+
+    document.getElementById('death').addEventListener('click', () => {
+        const deathAge = Math.floor(Math.random() * (120 - 60 + 1)) + 60;
+        result.textContent = `You will die at ${deathAge} years old.`;
+    });
+
+    document.getElementById('married').addEventListener('click', () => {
+        const marriedAge = Math.floor(Math.random() * (95-18 + 1)) + 18;
+        result.textContent = `You will get married when you are ${marriedAge} years old.`;
+    });
+
+    document.getElementById('dinner').addEventListener('click', () => {
+        const dinnerOptions = ['baseball field dirt', 'crickets', 'steak', 'brisket'];
+        const dinner = dinnerOptions[Math.floor(Math.random() * dinnerOptions.length)];
+        result.textContent = `You will have ${dinner} for dinner next Wednesday.`;
+    });
+
+    document.getElementById('car').addEventListener('click', () => {
+        const carOptions = ['hoopty', 'scooter', 'cadillac', 'bugatti'];
+        const car = carOptions[Math.floor(Math.random() * carOptions.length)];
+        result.textContent = `You will drive a ${car}.`;
+    });
+});
